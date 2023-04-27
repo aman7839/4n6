44bc913e60601f65d2ac9e741a29c60501780e67
rakshakgaind0910 <rakshakgaind0910@gmail.com>
2023-04-27 13:18:58 +0530

    sidebar updated

--- resources/views/admin/dashboard/layout/sidebar.blade.php
+++ resources/views/admin/dashboard/layout/sidebar.blade.php
@@ -8,9 +8,9 @@
 
             <div class="navbar-header">
 
-                <a class="navbar-brand" href="./"><img src="{{asset('/public/images/fanatic_logo.svg')}}" alt="Logo"></a>
+                <a class="navbar-brand" href="{{URL::to('/admin')}}"><img src="{{asset('/public/images/fanatic_logo.svg')}}" alt="Logo"></a>
 
-                <a class="navbar-brand hidden" href="./"><img src="{{asset('/public/images/logo2.png')}}" alt="Logo"></a>
+                <a class="navbar-brand hidden" href="{{URL::to('/admin')}}"><img src="{{asset('/public/images/logo2.png')}}" alt="Logo"></a>
 
                 {{-- <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a> --}}
 
